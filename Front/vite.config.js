d5f34c696d84ba5830b9dbf96100986ce4197084
Denis <dalexandre.semeru@gmail.com>
2025-07-19 15:38:44 +0200

    port

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    port: 5174, // ou n'importe quel port
+    host: '0.0.0.0' // pour accéder depuis d'autres machines
+  }
 })
