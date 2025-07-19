d4ca9596e5a3f5055573a257b205aee588a22448
Denis <dalexandre.semeru@gmail.com>
2025-07-19 16:50:44 +0200

    commit

--- Front/src/constants.js
+++ Front/src/constants.js
@@ -1,6 +1,9 @@
 // src/constants.js
 export const APP_NAME = 'Le restaurant de Chloé'
-export const SERVEUR = '192.168.1.122'
-export const SERVEUR_PORT = '3000'
-export const API_GET = 'http://' + SERVEUR + ':' + SERVEUR_PORT + '/reservations'
-export const API_POST = 'http://' + SERVEUR + ':' + SERVEUR_PORT + '/reservation'
+export const SERVEUR3 = 'localhost'
+export const SERVEUR1 = '192.168.1.122'
+export const SERVEUR = '213.44.161.12'
+export const SERVEUR_PORT_API = '40175'
+export const API_GET = 'http://' + SERVEUR + ':' + SERVEUR_PORT_API + '/reservations'
+export const API_POST = 'http://' + SERVEUR + ':' + SERVEUR_PORT_API + '/reservation'
+export const SERVEUR_PORT_FRONT = '40174'
--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { SERVEUR_PORT_FRONT } from './src/constants.js'
 
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 5174, // ou n'importe quel port
+    port: SERVEUR_PORT_FRONT, // ou n'importe quel port
     host: '0.0.0.0' // pour accéder depuis d'autres machines
   }
 })
