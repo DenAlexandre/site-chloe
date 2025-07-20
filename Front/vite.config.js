c50839f44cc873a8b9fdb66c73855c196cac9a67
Denis <dalexandre.semeru@gmail.com>
2025-07-20 11:26:14 +0200

    commit

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { SERVEUR_PORT_FRONT } from './src/constants.js'
+require('dotenv').config();
+
+const port = process.env.PORT || 40174;
 
 export default defineConfig({
   plugins: [vue()],
-allowedHosts: ['site-chloe.onrender.com']
+  server: {
+    port: port, // ou n'importe quel port
+    host: '0.0.0.0' // pour accéder depuis d'autres machines
+  },
+    allowedHosts: 'all'
 })
